a885d4570b4e7c6f1d6acf9baca38242d67bb0ab
Syed Mohsin <syedmohsindaniyalhaider@gmail.com>
2022-10-21 15:03:36 +0500

    #comment updated

--- components/Dashboard/Dashboard.tsx
+++ components/Dashboard/Dashboard.tsx
@@ -1,41 +1,95 @@
-import React from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import Image from "next/image";
 import Profile from "/assets/images/image5.svg";
-const Dashboard = () => {
-  const people = [
-    {
-      email: "lindsay.walton@example.com",
+import { Listbox, Menu, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/solid";
+import { GetServerSideProps } from "next";
+import { request } from "https";
 
-      image: Profile,
-    },
-    // More people...
+const classNames = (...classes: any) => {
+  return classes.filter(Boolean).join(" ");
+};
+
+const Dashboard = ({ data }: any) => {
+  const status = [
+    { id: 0, name: "Pending" },
+    { id: 1, name: "Accepted" },
+    { id: 2, name: "Rejected" },
   ];
+  const [selected, setSelected] = useState(null);
+  const [requests, setRequests] = useState(data);
+  const [activeUser, setActiveUser] = useState(requests[0]);
+  const [activeId, setActiveId] = useState(0);
+
+  const usersRequest = async () => {
+    const res = await fetch("https://lms-be-api.herokuapp.com/user");
+    const data = await res.json();
+    setRequests(data);
+  };
+
+  const createUserLogin = async () => {
+    console.log(selected);
+
+    const res = await fetch("http://lms-be-api.herokuapp.com/auth/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: activeUser.email }),
+    });
+    const data = await res.json();
+    console.log(data);
+  };
+
+  // const changeUserStatus = async () => {
+  //   if (activeId === activeUser.id) {
+  //     const user = {
+  //       id: activeUser?.id,
+  //       email: activeUser?.email,
+  //       status: selected,
+  //     };
+  //     const res = await fetch(
+  //       "https://lms-be-api.herokuapp.com/user/update-status",
+  //       {
+  //         method: "PATCH",
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //         },
+  //         body: JSON.stringify(user),
+  //       }
+  //     );
+
+  //     if (!res.ok) console.log("error");
+
+  //     usersRequest();
+  //   }
+
+  //   // createUserLogin(activeUser);
+  // };
+
+  useEffect(() => {
+    // changeUserStatus();
+    if (selected !== null) createUserLogin();
+  }, [selected]);
+
   return (
     <div className="px-4 sm:px-6 lg:px-8">
-      <div className="sm:flex sm:items-center">
-        <div className="sm:flex-auto">
-          <h1 className="text-xl font-semibold text-gray-900">Users</h1>
-          <p className="mt-2 text-sm text-gray-700">
-            A list of all the users in your account including their name, title,
-            email and role.
-          </p>
-        </div>
-        <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-          <button
-            type="button"
-            className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
-          >
-            Add user
-          </button>
-        </div>
+      <div className="sm:flex-auto">
+        <h1 className="text-xl font-semibold text-gray-900">Dashboard</h1>
       </div>
       <div className="mt-8 flex flex-col">
-        <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="-my-2 -mx-4 sm:-mx-6 lg:-mx-8">
           <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-            <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+            <div className="shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
               <table className="min-w-full divide-y divide-gray-300">
                 <thead className="bg-gray-50">
                   <tr>
+                    <th
+                      scope="col"
+                      className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                    >
+                      Sr#
+                    </th>
                     <th
                       scope="col"
                       className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
@@ -57,30 +111,48 @@ const Dashboard = () => {
                   </tr>
                 </thead>
                 <tbody className="divide-y divide-gray-200 bg-white">
-                  {people.map((person) => (
-                    <tr key={person.email}>
+                  {requests.map((ele: any) => (
+                    <tr key={ele.id}>
                       <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
                         <div className="flex items-center">
                           <div className="">
                             <div className="font-medium text-gray-900">
-                              {person.email}
+                              {ele.id}
+                            </div>
+                          </div>
+                        </div>
+                      </td>
+                      <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
+                        <div className="flex items-center">
+                          <div className="">
+                            <div className="font-medium text-gray-900">
+                              {ele.email}
                             </div>
                           </div>
                         </div>
                       </td>
                       <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
                         <span className="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
-                          Pending
+                          {ele.status}
                         </span>
                       </td>
 
                       <td className="relative whitespace-nowrap py-4 pl-3 pr-4  text-sm font-medium sm:pr-6">
-                        <a
-                          href="#"
-                          className="text-indigo-600  hover:text-indigo-900"
+                        <select
+                          className="mt-1 block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                          defaultValue={status[0].name}
+                          onChange={(e: any) => {
+                            setSelected(e.target.value);
+                            setActiveId(ele.id);
+                            setActiveUser(ele);
+                          }}
                         >
-                          Edit
-                        </a>
+                          {status.map((item) => (
+                            <option key={item.id} value={item.name}>
+                              {item.name}
+                            </option>
+                          ))}
+                        </select>
                       </td>
                     </tr>
                   ))}
--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import Dashboard from "../../components/Dashboard/Dashboard";
+import { GetServerSideProps } from "next";
 
-const index = () => {
-  return <Dashboard />;
+const index = ({ data }: any) => {
+  return <Dashboard data={data} />;
 };
+
+export async function getServerSideProps() {
+  const res = await fetch(`https://lms-be-api.herokuapp.com/requests`);
+  const data = await res.json();
+  return { props: { data } };
+}
 
 export default index;
